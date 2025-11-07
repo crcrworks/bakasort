77a506ce1f9638a8ca6200d2c38af7c09eb25e67
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-11-07 15:46:16 +0900

    edit BakaSort

--- crates/bakasort-core/src/lib.rs
+++ crates/bakasort-core/src/lib.rs
@@ -1,9 +1,7 @@
 pub trait BakaSort {
     type Item: PartialOrd + Ord;
-    fn step(v: Vec<Self::Item>) -> Vec<Self::Item>;
-}
-
-pub trait AsyncBakaSort {
-    type Item: PartialOrd + Ord;
-    fn step(v: Vec<Self::Item>) -> impl Future<Output = Vec<Self::Item>>;
+    fn step<T, U>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> U
+    where
+        T: IntoIterator<Item = Self::Item>,
+        U: Iterator<Item = Self::Item>;
 }
