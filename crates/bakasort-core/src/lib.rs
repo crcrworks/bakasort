dcd18e28057896582c2ad1f32ff43ea0d6aacf7e
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-11-07 15:49:01 +0900

    fix BakaSort trait

--- crates/bakasort-core/src/lib.rs
+++ crates/bakasort-core/src/lib.rs
@@ -1,7 +1,6 @@
 pub trait BakaSort {
     type Item: PartialOrd + Ord;
-    fn step<T, U>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> U
+    fn step<T>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> Vec<Self::Item>
     where
-        T: IntoIterator<Item = Self::Item>,
-        U: Iterator<Item = Self::Item>;
+        T: IntoIterator<Item = Self::Item>;
 }
