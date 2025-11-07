7811f72dc9a8fcba1e395d75f8dbfec06c2193f0
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-11-07 15:49:01 +0900

    rename BakaSort step into sort

--- crates/bakasort-core/src/lib.rs
+++ crates/bakasort-core/src/lib.rs
@@ -1,6 +1,6 @@
 pub trait BakaSort {
     type Item: PartialOrd + Ord;
-    fn step<T>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> Vec<Self::Item>
+    fn sort<T>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> Vec<Self::Item>
     where
         T: IntoIterator<Item = Self::Item>;
 }
