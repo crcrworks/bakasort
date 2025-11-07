1e32e5721de1af23f7787204d96c9275539960f7
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-11-07 15:49:01 +0900

    feat: implement putting stones

--- crates/stalin-sort/src/lib.rs
+++ crates/stalin-sort/src/lib.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use bakasort_core::BakaSort;
+
+struct StalinSort {}
+
+impl BakaSort for StalinSort {
+    type Item = i32;
+
+    fn sort<T>(array: T, sender: flume::Sender<Vec<Self::Item>>) -> Vec<Self::Item>
+    where
+        T: IntoIterator<Item = Self::Item>,
+    {
+        let mut result = Vec::new();
+        let mut max = Self::Item::MIN;
+
+        for i in array {
+            if max < i {
+                max = i;
+                result.push(i);
+            }
+
+            let _ = sender.send(result.clone());
+        }
+
+        result
+    }
 }
